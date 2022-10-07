e4ac03df4098b87ab163cf81999a0412c55e88f8
umeshawijesingha <umeshawijesingha@gmail.com>
2022-10-07 23:14:20 +0530

    Springboot-react REST API backend created

---
+++ src/main/java/com/studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.studentsystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studentsystem.model.Student;
+import com.studentsystem.repository.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public Student saveStudent(Student student) {
+		
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public List<Student> getAllStudent() {
+		
+		return studentRepository.findAll();
+	}
+	
+
+}
